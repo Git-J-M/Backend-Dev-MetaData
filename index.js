676494208f91d6598b8582baa417d3ac8ec8da88
Paarthurax <privatedndemail@gmail.com>
2025-02-05 12:51:27 +0100

    Implemented multer to help with uploads

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+require('dotenv').config();
+var multer = require('multer');
+var bodyParser = require('body-parser');
 var app = express();
+
+var upload = multer({ dest: 'uploads/'})
+
+app.use(bodyParser.json() );      
+app.use(bodyParser.urlencoded({     
+  extended: true
+}));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,9 +20,25 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+    
+  if(!req.file){
+
+    return res.status(500).json({Error: 'No file uploaded!'});
+    
+  }
+
+  console.log(req.file);
+
+  res.json({
+    name: req.file.originalname,
+
+    type: req.file.mimetype, 
+    size: req.file.size 
+  });
+})
